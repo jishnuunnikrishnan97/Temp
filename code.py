d31f7123676855487bf2dd93359a969e8f578c91
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-11-27 15:23:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,5 @@
-def replace_single_newline(input_string):
-    # Use regular expression to find consecutive "\n" and count them
-    matches = re.findall('\n+', input_string)
-    
-    # Replace single "\n" with a space if there is no consecutive occurrence
-    cleaned_string = re.sub('(?<!\n)\n(?!\n)', ' ', input_string)
+def preserve_newline_after_period(input_string):
+    # Use regular expression to replace "\n" not after "."
+    cleaned_string = re.sub('(?<!\.)\n(?! )', ' ', input_string)
 
     return cleaned_string
