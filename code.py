8cbf656b5b6c7396bfb3254e9e253e2f307a1004
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-11-27 15:31:27 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,19 @@
-def preserve_newline_after_period(input_string):
-    # Use regular expression to replace "\n" not after "."
-    cleaned_string = re.sub('(?<!\.)\n(?! )', ' ', input_string)
+import re
+
+def custom_newline_replacement(input_string):
+    # Identify and store occurrences of "ARTICLE" between two "\n"
+    article_matches = re.findall('\nARTICLE\n', input_string)
+
+    # Replace "\n" not after "." with a space, ignoring spaces between "." and "\n"
+    cleaned_string = re.sub('(?<!\.)\n(?!ARTICLE\n)', ' ', input_string)
+
+    # Restore occurrences of "ARTICLE" between two "\n"
+    for match in article_matches:
+        cleaned_string = cleaned_string.replace(match, '\nARTICLE\n')
 
     return cleaned_string
+
+# Example usage:
+input_string = "This is a\nsample.\n\nARTICLE\nText with\nnewlines.\nAnother.\nARTICLE\nMore text.\n"
+result = custom_newline_replacement(input_string)
+print(result)
