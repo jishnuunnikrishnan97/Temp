4ed515b6a065984c0cd9a00c6710e2b37100e4b0
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-11-27 15:43:34 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,15 @@
 import re
 
-def replace_newlines_with_conditions(input_string):
-    # Use regular expression to replace "\n" based on conditions
-    cleaned_string = re.sub(r'(?<=[.])\s*\n\s*|\n(?=ARTICLE)', ' ', input_string)
+def modify_string(input_string):
+    # Replace "\n" with space if there is no "." before it
+    cleaned_string = re.sub('(?<!\.)\n(?! )', ' ', input_string)
+
+    # Add "." before the last "\n" after "ARTICLE" between two "\n"
+    cleaned_string = re.sub('\nARTICLE\n', '.\n', cleaned_string)
 
     return cleaned_string
 
 # Example usage:
-input_string = "This is a sample.\n\nARTICLE\nwith newlines.\nAnother\n\nARTICLE\nsection."
-result = replace_newlines_with_conditions(input_string)
+input_string = "This is a\nsample ARTICLE\nstring\nwith\nnewlines.\n"
+result = modify_string(input_string)
 print(result)
