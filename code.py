681ac0c91e6cb41ddfdf088c67ff107657e6f425
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-11-27 15:52:24 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,19 @@
 import re
 
-def modify_string(input_string):
-    # Replace "\n" with space if there is no "." before it
-    cleaned_string = re.sub('(?<!\.)\n(?! )', ' ', input_string)
+def modify_article_string(input_string):
+    # Use regular expression to find "ARTICLE" between two "\n"
+    article_pattern = re.compile(r'\n(.*ARTICLE.*\n)(.*\n)', re.DOTALL)
 
-    # Add "." before the last "\n" after "ARTICLE" between two "\n"
-    cleaned_string = re.sub('\nARTICLE\n', '.\n', cleaned_string)
+    # Check if the pattern is present and modify the string accordingly
+    match = article_pattern.search(input_string)
+    if match:
+        modified_string = input_string.replace(match.group(0), match.group(1).rstrip('.') + '.\n' + match.group(2).replace('\n', ' '))
+    else:
+        modified_string = re.sub('(?<!\.)\n(?! )', ' ', input_string)
 
-    return cleaned_string
+    return modified_string
 
 # Example usage:
-input_string = "This is a\nsample ARTICLE\nstring\nwith\nnewlines.\n"
-result = modify_string(input_string)
+input_string = "\nArticle 2. - fund replacement\nThis is a\nsample ARTICLE string\nwith\nnewlines.\nAnother ARTICLE example.\n"
+result = modify_article_string(input_string)
 print(result)
