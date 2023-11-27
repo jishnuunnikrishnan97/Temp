783719303fe90229d7f05223e475e496e1a0e0a4
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-11-27 16:02:47 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,15 @@
 import re
 
 def process_string(input_string):
-    # Define a regular expression pattern to match "\n" conditions
-    pattern = re.compile(r'(?<=\.)\n|(?<=\.\s)\n|\n(\d+|\s*\d+)?')
+    # Define a regular expression pattern to identify "\n" occurrences based on the given rules
+    pattern = re.compile(r'(?<!\.)\n(?!\d|ARTICLE)')
+    
+    # Replace "\n" occurrences based on the defined pattern
+    output_string = re.sub(pattern, ' ', input_string)
+    
+    return output_string
 
-    # Use the pattern to replace "\n" based on the specified conditions
-    result_string = re.sub(pattern, lambda match: match.group(0) if match.group(1) else ' ', input_string)
-
-    return result_string
-
-# Example usage
-input_str = "This is a string. It has some\nnewlines.\nThis one is after a number.\n1\nThis is another line."
-output_str = process_string(input_str)
-
-print(output_str)
+# Example usage:
+input_string = "Your long input string here."
+result = process_string(input_string)
+print(result)
