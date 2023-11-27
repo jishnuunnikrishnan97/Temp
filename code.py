2407df34d722df82f338214e008218adfee477c9
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-11-27 15:03:10 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,8 @@
-import re
+def replace_single_newline(input_string):
+    # Use regular expression to find consecutive "\n" and count them
+    matches = re.findall('\n+', input_string)
+    
+    # Replace single "\n" with a space if there is no consecutive occurrence
+    cleaned_string = re.sub('(?<!\n)\n(?!\n)', ' ', input_string)
 
-def modify_newlines(input_text):
-    # Define the pattern to match the recurring lines
-    pattern = re.compile(r'\nversion: Aug 2006 page \d+ of \d+\n')
-
-    # Use the pattern to replace matching lines with an empty string
-    cleaned_text = re.sub(pattern, '', input_text)
-
-    # Count consecutive "\n" and replace a single "\n" with a space
-    cleaned_text = re.sub(r'\n+', ' ', cleaned_text)
-
-    return cleaned_text
+    return cleaned_string
