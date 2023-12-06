ae9ea59d673db75329a4a54fd09bf90e6031b90d
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-06 09:54:15 +0530

    Update code.r

--- code.r
+++ code.r
@@ -1,5 +1,18 @@
-def split_paragraphs(text):
-    pattern = re.compile(r'\n\d+\.\s+[A-Za-z\s]+\\n')
-    paragraphs = pattern.split(text)
-    paragraphs = [p.strip() for p in paragraphs if p.strip()]
-    return paragraphs
+import re
+
+def remove_before_pattern(input_text):
+    pattern = re.compile(r'.*?(\n\d+\.\s(?:Terms|Terminologies)\s*\n)')
+
+    match = re.search(pattern, input_text)
+
+    if match:
+        start_index = match.end(1)
+        result = input_text[start_index:]
+        return result
+    else:
+        return input_text
+
+# Example usage:
+input_text = "Some text before\n3. Terms \nYour content here."
+result = remove_before_pattern(input_text)
+print(result)
