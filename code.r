89134ae31ef92c0faa3a6bdb60af3c10dc88e04d
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-06 11:33:38 +0530

    Update code.r

--- code.r
+++ code.r
@@ -1,8 +1,1 @@
-def search_string_in_paragraphs(input_text, search_string):
-    paragraphs = split_paragraphs(input_text)
-
-    for paragraph in paragraphs:
-        if search_string in paragraph['heading'] or search_string in paragraph['paragraph']:
-            return paragraph
-
-    return None
+r'\n\d+\.\s*[\w\s.,]+\.?\s*\n'
