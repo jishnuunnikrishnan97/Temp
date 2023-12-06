6a35125292b3443986d933fa705e261b449fe9a2
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-06 09:49:31 +0530

    Update code.r

--- code.r
+++ code.r
@@ -1,17 +1,5 @@
-import re
-
-def extract_pattern(text):
-    pattern = re.compile(r'\n\d+\.\s\w+\s*\n')
-    match = pattern.search(text)
-
-    if match:
-        start_index = match.end()
-        result = text[start_index:]
-        return result
-    else:
-        return "Pattern not found in the given string."
-
-# Example usage:
-input_text = "Some text before\n3. Terms \nThis is the content you want to keep."
-result = extract_pattern(input_text)
-print(result)
+def split_paragraphs(text):
+    pattern = re.compile(r'\n\d+\.\s+[A-Za-z\s]+\\n')
+    paragraphs = pattern.split(text)
+    paragraphs = [p.strip() for p in paragraphs if p.strip()]
+    return paragraphs
