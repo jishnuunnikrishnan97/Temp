4d52cff9128dcdf914e43495c35804888aad99b9
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-06 09:45:03 +0530

    Update code.r

--- code.r
+++ code.r
@@ -1,20 +1,17 @@
 import re
 
-def split_paragraphs(input_text):
-    pattern = re.compile(r'\n\d+\.\s\w+')
+def extract_pattern(text):
+    pattern = re.compile(r'\n\d+\.\s\w+\s*\n')
+    match = pattern.search(text)
 
-    paragraphs = pattern.split(input_text)
-    headings = pattern.findall(input_text)
-
-    # Remove empty strings from the list
-    paragraphs = [para.strip() for para in paragraphs if para.strip()]
-
-    # Combine paragraphs and headings
-    result = [{'heading': heading, 'paragraph': paragraph} for heading, paragraph in zip(headings, paragraphs)]
-
-    return result
+    if match:
+        start_index = match.end()
+        result = text[start_index:]
+        return result
+    else:
+        return "Pattern not found in the given string."
 
 # Example usage:
-input_text = "\n1. Definition Some text here.\n2. Terms More text for terms.\n3. Indemnification Final text for indemnification."
-result = split_paragraphs(input_text)
+input_text = "Some text before\n3. Terms \nThis is the content you want to keep."
+result = extract_pattern(input_text)
 print(result)
