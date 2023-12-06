d0a79d6d3a7e70a328ddedd9fc6f6a02c7a3831a
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-06 10:27:02 +0530

    Update code.r

--- code.r
+++ code.r
@@ -1,7 +1,7 @@
 def extract_paragraph(text):
-    pattern = re.compile(r'.*?(\n\d+\.\s*Term.*?)(?=\n\d+\.\s*Term|\Z)', re.DOTALL)
+    pattern = re.compile(r'\n\d+\.\s*Term.*?(?=\n\d+\.\s*Term|\Z)', re.DOTALL)
     match = pattern.search(text)
     if match:
-        return match.group(1)
+        return match.group(0)
     else:
         return None
