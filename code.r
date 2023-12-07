f395164f2e64c051816b69b02d21d4c94a63d68e
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-07 23:27:29 +0530

    Update code.r

--- code.r
+++ code.r
@@ -1,19 +1,19 @@
-def search_strings_in_text(input_text, search_strings):
+def search_strings(input_text, search_strings):
     result_dict = {}
 
     paragraphs = split_paragraphs(input_text)
 
-    for search_str in search_strings:
-        search_str_lower = search_str.lower()
+    for search_term in search_strings:
+        search_term_lower = search_term.lower()
         matching_paragraphs = []
 
         for entry in paragraphs:
             heading_lower = entry['heading'].lower()
             paragraph_lower = entry['paragraph'].lower()
 
-            if search_str_lower in heading_lower or search_str_lower in paragraph_lower:
+            if search_term_lower in heading_lower or search_term_lower in paragraph_lower:
                 matching_paragraphs.append(entry['paragraph'])
 
-        result_dict[search_str] = matching_paragraphs
+        result_dict[search_term] = matching_paragraphs
 
     return result_dict
