d16f27f663f11d93674de173f0241a918a8d0671
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-08 17:15:25 +0530

    Update xl_script.r

--- xl_script.r
+++ xl_script.r
@@ -1,5 +1,23 @@
 import pandas as pd
 
 # Assuming your DataFrame is named 'df'
-df = df.dropna(subset=['SSN'])  # Remove rows where 'SSN' is NaN
-df = df[df['SSN'].astype(str).str.len() == 10]  # Keep rows with 'SSN' length equal to 10
+# Step 1
+df['amt_sum'] = df.groupby(['Cust ID', 'SSN', 'rating'])['amt'].transform('sum')
+df = df[df.duplicated(['Cust ID', 'SSN', 'rating'], keep=False) | ~df.duplicated(['Cust ID', 'SSN', 'rating'])]
+
+# Step 2
+unique_ratings = df['rating'].nunique()
+df = pd.concat([df, pd.get_dummies(df['rating'])], axis=1)
+
+# Step 3
+for rating in df['rating'].unique():
+    mask = (df.duplicated(['Cust ID', 'SSN']) & (df['rating'] == rating))
+    df[rating] = df.loc[mask, 'amt']
+    df.loc[mask, 'amt'] = df.loc[mask, 'amt'].sum()
+df = df[~df.duplicated(['Cust ID', 'SSN'])]
+
+# Step 4
+df = df.drop('rating', axis=1)
+
+# Display the resulting DataFrame
+print(df)
