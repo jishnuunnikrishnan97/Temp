af8c4d9ba142b4ad50ba4d8b77c6c7b9f210b551
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-08 15:14:04 +0530

    Update xl_script.r

--- xl_script.r
+++ xl_script.r
@@ -1,11 +1,14 @@
-import os
+import pandas as pd
 
-def remove_extension(file_path):
-    base_name = os.path.basename(file_path)
-    file_name, _ = os.path.splitext(base_name)
-    return file_name
+def format_column_to_three_digits(input_excel, output_excel, column_name):
+    # Read Excel file into DataFrame
+    df = pd.read_excel(input_excel)
 
-# Example usage
-file_path = '/path/to/your/file/example.txt'
-result = remove_extension(file_path)
-print(result)
+    # Format the specified column to have three digits
+    df[column_name] = df[column_name].astype(str).str.zfill(3)
+
+    # Write the modified DataFrame to a new Excel file
+    df.to_excel(output_excel, index=False)
+
+# Replace 'input.xlsx', 'output.xlsx', and 'YourColumnName' with actual file paths and column name
+format_column_to_three_digits('input.xlsx', 'output.xlsx', 'YourColumnName')
