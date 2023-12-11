1fc7a67f5db88df84bb7fca7ce715b0445bb47de
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-11 17:30:25 +0530

    Update xl_script.r

--- xl_script.r
+++ xl_script.r
@@ -1,24 +1,22 @@
-import pandas as pd
+df = pd.DataFrame(data)
 
-# Assuming your DataFrame is named df
-# Task 1
+# Task 1: Summing amounts for rows with the same Cust ID, SSN, rating
 df['amt'] = df.groupby(['Cust ID', 'SSN', 'rating'])['amt'].transform('sum')
 df = df.drop_duplicates(['Cust ID', 'SSN', 'rating']).reset_index(drop=True)
 
-# Task 2
-unique_ratings = df['rating'].nunique()
-df = pd.concat([df, pd.DataFrame(0, index=df.index, columns=[f'rating_{i}' for i in range(unique_ratings)])], axis=1)
+# Task 2: Create new columns with unique values in the rating column
+unique_ratings = df['rating'].unique()
+for rating in unique_ratings:
+    df[rating] = 0
 
-# Task 3
+# Task 3: Update columns based on different ratings
 for index, row in df.iterrows():
-    same_cust_ssn = df[(df['Cust ID'] == row['Cust ID']) & (df['SSN'] == row['SSN'])]
-    if len(same_cust_ssn['rating'].unique()) > 1:
-        for rating in same_cust_ssn['rating'].unique():
-            df.loc[index, f'rating_{rating}'] = same_cust_ssn[same_cust_ssn['rating'] == rating]['amt'].sum()
-        df = df.drop(same_cust_ssn.index.difference([index]))
+    for rating in unique_ratings:
+        if row['rating'] == rating:
+            df.at[index, rating] = row['amt']
 
-# Task 4
-df = df.drop('rating', axis=1)
+# Task 4: Remove the 'rating' column
+df = df.drop(columns=['rating'])
 
-# Display the resulting DataFrame
+# Displaying the final dataframe
 print(df)
