aa0d2f13765c2449dc03969f7fca4a2e92db6f0d
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-11 13:41:35 +0530

    Update file_search in folder.r

--- file_search in folder.r
+++ file_search in folder.r
@@ -1,1 +1,7 @@
+def find_excel_file(folder_path):
+    for file_name in os.listdir(folder_path):
+        if file_name.startswith("Corporate Ratings Working") and file_name.endswith(".xlsx"):
+            return os.path.join(folder_path, file_name)
 
+    # Return None if no matching file is found
+    return None
