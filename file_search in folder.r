65715cf2f5760585ca36d984a93e3cbd667bfb60
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-11 14:53:40 +0530

    Update file_search in folder.r

--- file_search in folder.r
+++ file_search in folder.r
@@ -17,3 +17,29 @@ def find_excel_file(folder_path, start_of_filename):
 
     except Exception as e:
         return f"Error: {str(e)}"
+
+
+
+
+
+def read_sheet_by_name_start(excel_file, start_of_sheet_name):
+    try:
+        # Read all sheet names from the Excel file
+        all_sheet_names = pd.ExcelFile(excel_file).sheet_names
+
+        # Define the regex pattern for matching the sheet name
+        pattern = re.compile(f"{re.escape(start_of_sheet_name)}.*", re.IGNORECASE)
+
+        # Find the first matching sheet name
+        matching_sheet = next((sheet for sheet in all_sheet_names if pattern.match(sheet)), None)
+
+        # If a matching sheet is found, create a dataframe; otherwise, print a message
+        if matching_sheet:
+            df = pd.read_excel(excel_file, sheet_name=matching_sheet)
+            return df
+        else:
+            print("No matching sheet name found.")
+            return None
+
+    except Exception as e:
+        return f"Error: {str(e)
