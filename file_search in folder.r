cb7f3235124d6bd51486ae8ffb89b83dee2f9ae4
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-11 15:05:59 +0530

    Update file_search in folder.r

--- file_search in folder.r
+++ file_search in folder.r
@@ -22,24 +22,22 @@ def find_excel_file(folder_path, start_of_filename):
 
 
 
-def read_sheet_by_name_start(excel_file, start_of_sheet_name):
+def find_sheet_in_excel(excel_file, start_of_sheet_name):
     try:
-        # Read all sheet names from the Excel file
-        all_sheet_names = pd.ExcelFile(excel_file).sheet_names
+        # Read the Excel file into a dictionary of DataFrames (each DataFrame represents a sheet)
+        excel_sheets = pd.read_excel(excel_file, sheet_name=None)
 
         # Define the regex pattern for matching the sheet name
-        pattern = re.compile(f"{re.escape(start_of_sheet_name)}.*", re.IGNORECASE)
+        pattern = re.compile(f"^{re.escape(start_of_sheet_name)}", re.IGNORECASE)
 
-        # Find the first matching sheet name
-        matching_sheet = next((sheet for sheet in all_sheet_names if pattern.match(sheet)), None)
+        # Filter the sheet names based on the regex pattern
+        matching_sheets = [sheet for sheet in excel_sheets.keys() if pattern.match(sheet)]
 
-        # If a matching sheet is found, create a dataframe; otherwise, print a message
-        if matching_sheet:
-            df = pd.read_excel(excel_file, sheet_name=matching_sheet)
-            return df
+        # If there are matching sheets, return the first one; otherwise, print a message
+        if matching_sheets:
+            return matching_sheets[0]
         else:
-            print("No matching sheet name found.")
-            return None
+            return "No sheet name found."
 
     except Exception as e:
-        return f"Error: {str(e)
+        return f"Error: {str(e)}"
