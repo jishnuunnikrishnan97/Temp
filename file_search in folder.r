4fdbb04ecc3675b1b6c981fc2427d71a4df608b0
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-11 13:57:29 +0530

    Update file_search in folder.r

--- file_search in folder.r
+++ file_search in folder.r
@@ -1,7 +1,19 @@
-def find_excel_file(folder_path):
-    for file_name in os.listdir(folder_path):
-        if file_name.startswith("Corporate Ratings Working") and file_name.endswith(".xlsx"):
-            return os.path.join(folder_path, file_name)
+def find_excel_file(folder_path, start_of_filename):
+    try:
+        # List all files in the specified folder
+        files = os.listdir(folder_path)
 
-    # Return None if no matching file is found
-    return None
+        # Define the regex pattern for matching the filename
+        pattern = re.compile(f"{re.escape(start_of_filename)}.*\.xlsx", re.IGNORECASE)
+
+        # Filter the files based on the regex pattern
+        matching_files = [file for file in files if pattern.match(file)]
+
+        # If there are matching files, return the first one; otherwise, return None
+        if matching_files:
+            return os.path.join(folder_path, matching_files[0])
+        else:
+            return None
+
+    except Exception as e:
+        return f"Error: {str(e)}"
