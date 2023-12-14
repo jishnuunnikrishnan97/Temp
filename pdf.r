738f7ee242b36cc42a9b0d29b284606b123c1e0b
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-14 13:24:48 +0530

    Update pdf.r

--- pdf.r
+++ pdf.r
@@ -1,25 +1,5 @@
-import fitz
-
-def extract_images_from_pdf(pdf_path, image_folder):
-    doc = fitz.open(pdf_path)
-    
-    for page_number in range(doc.page_count):
-        page = doc[page_number]
-        images = page.get_images(full=True)
-        
-        for img_index, img_info in enumerate(images):
-            img_index += 1
-            base_image = doc.extract_image(img_info[0])
-            image_bytes = base_image["image"]
-            
-            image_filename = f"{image_folder}/page{page_number + 1}_img{img_index}.png"
-            
-            with open(image_filename, "wb") as image_file:
-                image_file.write(image_bytes)
-
-    doc.close()
-
-# Example usage:
-pdf_path = "your_pdf.pdf"
-output_folder = "output_images"
-extract_images_from_pdf(pdf_path, output_folder)
+def get_file_timestamp(file_path):
+    timestamp = os.path.getmtime(file_path)
+    # Convert timestamp to a human-readable format
+    formatted_timestamp = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    return formatted_timestamp
