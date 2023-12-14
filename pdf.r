3a94e39e564910485c80befc7219e4bd51a34054
Jishnu Unnikrishnan <63056796+jishnuunnikrishnan97@users.noreply.github.com>
2023-12-14 11:31:31 +0530

    Update

---
+++ pdf.r
@@ -0,0 +1,25 @@
+import fitz
+
+def extract_images_from_pdf(pdf_path, image_folder):
+    doc = fitz.open(pdf_path)
+    
+    for page_number in range(doc.page_count):
+        page = doc[page_number]
+        images = page.get_images(full=True)
+        
+        for img_index, img_info in enumerate(images):
+            img_index += 1
+            base_image = doc.extract_image(img_info)
+            image_bytes = base_image["image"]
+            
+            image_filename = f"{image_folder}/page{page_number + 1}_img{img_index}.png"
+            
+            with open(image_filename, "wb") as image_file:
+                image_file.write(image_bytes)
+
+    doc.close()
+
+# Example usage:
+pdf_path = "your_pdf.pdf"
+output_folder = "output_images"
+extract_images_from_pdf(pdf_path, output_folder)
